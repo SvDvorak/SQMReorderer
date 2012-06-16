e75463b77908bc6114615e358e2241ede423ca3f
Andreas Wilcox <wilcox@m-sys.se>
2012-06-17 01:03:23 +0200

    Started implementation of Groups parser.

---
+++ SQMReorderer/SqmParser/Parsers/GroupsParser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQMReorderer.SqmParser.Parsers
+{
+    public class GroupsParser
+    {
+        public Group ParseGroupElement(SqmStream stream)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Group
+    {
+        public List<Item> Items { get; set; }
+    }
+}

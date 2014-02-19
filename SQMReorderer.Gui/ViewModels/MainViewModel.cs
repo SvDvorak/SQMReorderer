5b4e5cca891ce37807f32fcd9f01aed3034448b9
Andreas Wilcox <UnknownBum@gmail.com>
2014-02-19 17:11:29 +0100

    Implemented new viewmodel structure

--- SQMReorderer.Gui/ViewModels/MainViewModel.cs
+++ SQMReorderer.Gui/ViewModels/MainViewModel.cs
@@ -74,8 +74,10 @@ namespace SQMReorderer.Gui.ViewModels
 
             if(_sqmContents != null)
             {
-                var sqmViewModelCreator = new SqmViewModelCreator();
-                Mission = sqmViewModelCreator.CreateMissionViewModel(_sqmContents.Mission);
+                //var sqmViewModelCreator = new SqmViewModelCreator();
+                //Mission = sqmViewModelCreator.CreateMissionViewModel(_sqmContents.Mission);
+                var teamViewModelsFactory = new TeamViewModelsFactory(new GroupViewModelsFactory(new VehicleViewModelsFactory()));
+                Teams = teamViewModelsFactory.Create(_sqmContents.Mission.Groups);
             }
         }
 

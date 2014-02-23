c9ab15b97df1950be287d0900d37bebaf4eb226a
Andreas Wilcox <UnknownBum@gmail.com>
2014-02-23 01:39:47 +0100

    Fixed main viewmodel to use correct classes

--- SQMReorderer.Gui/ViewModels/MainViewModel.cs
+++ SQMReorderer.Gui/ViewModels/MainViewModel.cs
@@ -1,10 +1,14 @@
 using System.Collections.Generic;
 using System.Linq;
+using SQMReorderer.Core.Export;
+using SQMReorderer.Core.Import;
 using SQMReorderer.Core.Import.Context;
+using SQMReorderer.Core.Import.FileVersion;
 using SQMReorderer.Core.Import.ResultObjects;
 using SQMReorderer.Core.StreamHelpers;
 using SQMReorderer.Gui.Command;
 using SQMReorderer.Gui.Dialogs;
+using SqmFileExporter = SQMReorderer.Core.Export.SqmFileExporter;
 
 namespace SQMReorderer.Gui.ViewModels
 {
@@ -22,6 +26,7 @@ namespace SQMReorderer.Gui.ViewModels
         public DelegateCommand SaveAsCommand { get; private set; }
 
         private IEnumerable<TeamViewModel> _teams;
+
         public IEnumerable<TeamViewModel> Teams
         {
             get { return _teams; }
@@ -29,6 +34,7 @@ namespace SQMReorderer.Gui.ViewModels
         }
 
         private IEnumerable<object> _selectedItems;
+
         public IEnumerable<object> SelectedItems
         {
             get { return _selectedItems; }
@@ -44,40 +50,50 @@ namespace SQMReorderer.Gui.ViewModels
         }
 
         private CombinedVehicleViewModel _selectedItemsViewModel;
+
         public CombinedVehicleViewModel SelectedItemsViewModel
         {
             get { return _selectedItemsViewModel; }
-            set
-            {
-                Set(value, () => SelectedItemsViewModel, () => _selectedItemsViewModel = value);
-            }
+            set { Set(value, () => SelectedItemsViewModel, () => _selectedItemsViewModel = value); }
         }
 
         private void OpenFile()
         {
             var streamToStringsReader = new StreamToStringsReader();
             var sqmContextCreator = new SqmContextCreator();
-            var arma2Importer = new Core.Import.ArmA2.SqmFileImporter(streamToStringsReader, sqmContextCreator, new Core.Import.ArmA2.SqmParser());
-            var arma3Importer = new Core.Import.ArmA3.SqmFileImporter(streamToStringsReader, sqmContextCreator, new Core.Import.ArmA3.SqmParser());
-            var openSqmFileDialog = new OpenSqmFileDialog(new OpenFileDialogAdapter(), new Core.Import.SqmFileImporter(null, null, arma2Importer, arma3Importer));
+            var arma2Importer = new Core.Import.ArmA2.SqmFileImporter(streamToStringsReader, sqmContextCreator,
+                new Core.Import.ArmA2.SqmParser());
+            var arma3Importer = new Core.Import.ArmA3.SqmFileImporter(streamToStringsReader, sqmContextCreator,
+                new Core.Import.ArmA3.SqmParser());
+            var openSqmFileDialog = new OpenSqmFileDialog(new OpenFileDialogAdapter(),
+                new SqmFileImporter(new FileVersionRetriever(new StreamReaderFactory()), new SqmContentCombiner(),
+                    arma2Importer, arma3Importer));
 
             _sqmContents = openSqmFileDialog.ShowDialog();
 
-            if(_sqmContents != null)
+            if (_sqmContents != null)
             {
-                var teamViewModelsFactory = new TeamViewModelsFactory(new GroupViewModelsFactory(new VehicleViewModelsFactory()));
+                var teamViewModelsFactory =
+                    new TeamViewModelsFactory(new GroupViewModelsFactory(new VehicleViewModelsFactory()));
                 Teams = teamViewModelsFactory.Create(_sqmContents.Mission.Groups);
             }
         }
 
         private void SaveFileAs()
         {
-            //var saveSqmFileDialog = new SaveSqmFileDialog(new SaveFileDialogAdapter(), new SqmFileExporter(new SqmElementExportVisitor(), new ContextIndenter(), new StreamWriterFactory()));
+            var contextIndenter = new ContextIndenter();
+            var streamWriterFactory = new StreamWriterFactory();
+            var saveSqmFileDialog = new SaveSqmFileDialog(new SaveFileDialogAdapter(), new SqmFileExporter(
+                new Core.Export.ArmA2.SqmFileExporter(new Core.Export.ArmA2.SqmElementExportVisitor(),
+                    contextIndenter, streamWriterFactory),
+                new Core.Export.ArmA3.SqmFileExporter(new Core.Export.ArmA3.SqmElementExportVisitor(),
+                    contextIndenter, streamWriterFactory),
+                new FileVersionRetriever(new StreamReaderFactory())));
 
-            //var reorderer = new ViewModelToContentReorderer();
-            //reorderer.Reorder(_sqmContents.Mission, Teams.ToList());
+            var reorderer = new ViewModelToContentReorderer();
+            reorderer.Reorder(_sqmContents.Mission, Teams.ToList());
 
-            //saveSqmFileDialog.ShowDialog(_sqmContents);
+            saveSqmFileDialog.ShowDialog(_sqmContents);
         }
     }
 }
